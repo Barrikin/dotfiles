ae938888eac28ac3224a87efea3586f2d9cd134b
Barrikin <54464128+Barrikin@users.noreply.github.com>
2022-11-29 04:08:21 -0500

    updates

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+ln -s $PWD/starship.toml ~/.config/starship.toml
 ln -s $PWD/.bash_profile ~/.bash_profile
 ln -s $PWD/.bashrc ~/.bashrc
 
-source ~/.bash_profile
+eval "$(source ~/.bash_profile)"
